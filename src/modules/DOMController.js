f5625e1f06c036652b69830b0864eb6eb5e2673c
PetraCore <102828971+PetraCore@users.noreply.github.com>
2024-04-29 21:28:42 +0200

    Display todo due date in more human format
    The user retains the ability to see exact due date in todo details

--- src/modules/DOMController.js
+++ src/modules/DOMController.js
@@ -1,3 +1,5 @@
+import { formatDistanceToNow } from "date-fns";
+
 export default class DOMController {
     #projectIconSVG = '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="white" d="M10 4H4c-1.11 0-2 .89-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2h-8z"/></svg>';
     #projectSelectedIconSVG = '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="white" d="M19 20H4a2 2 0 0 1-2-2V6c0-1.11.89-2 2-2h6l2 2h7a2 2 0 0 1 2 2H4v10l2.14-8h17.07l-2.28 8.5c-.23.87-1.01 1.5-1.93 1.5"/></svg>';
@@ -168,6 +170,10 @@ export default class DOMController {
         todoCard.dataset.parentProject = todo.parentProject;
         todoCard.classList.add('todo-card');
 
+        const dueDate = new Date(todo.dueDate);
+        const currentDate = new Date();
+        const isOverdue = (dueDate.getTime() - currentDate.getTime()) < 0;
+
         todoCard.innerHTML = `
             <div class="todo-checkbox"> 
                 ${todo.isCompleted ? this.#todoCheckFilledSVG : this.#todoCheckEmptySVG}
@@ -179,7 +185,15 @@ export default class DOMController {
                 ${todo.description} 
             </div>
             <div class="todo-due-date">
-                ${todo.dueDate} 
+                ${
+                    todo.isCompleted
+                    ? ''
+                    : !todo.dueDate
+                    ? ''
+                    : isOverdue
+                    ? 'overdue!' 
+                    : formatDistanceToNow(dueDate)
+                } 
             </div>
             <div class="todo-priority">
                 ${todo.priority} 
